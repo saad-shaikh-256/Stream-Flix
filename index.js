b0071ac44b042abe37a413bfe39a4d4bc4adc409
Saad Shaikh <shaikhsaad256@gmail.com>
2025-03-23 20:55:50 +0530

    Home Page GSAP

---
+++ index.js
@@ -0,0 +1,24 @@
+gsap.to("nav", {
+   y: "0%",
+   opacity: 1,
+   duration: 1.25,
+   ease: "power4.inOut",
+   delay: 0.5,
+   stagger: 0.15,
+});
+gsap.to("#movieDetails", {
+   y: "0%",
+   opacity: 1,
+   duration: 1.25,
+   ease: "power4.inOut",
+   delay: 0.5,
+   stagger: 0.15,
+});
+gsap.to("#buttons", {
+   y: "0%",
+   opacity: 1,
+   duration: 1.25,
+   ease: "power4.inOut",
+   delay: 1,
+   stagger: 0.15,
+});
